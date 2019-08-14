8ba06e4f4d4ee41d955112744bb68ded3e810ab0
Tibebeselasie Mehari <tibebes.js@gmail.com>
2019-08-14 08:52:14 -0400

    simple cleanup

--- scrapper/translationServer.js
+++ scrapper/translationServer.js
@@ -1,15 +1,13 @@
 const puppeteer = require('puppeteer');
-// const languages = require('./languages.json');
 const Express = require('express');
 
 const app = new Express();
 
+const PORT = 3002;
+
 app.get("/translate", async (req, res) => {
         let urlFromText = encodeURI(req.query.text);
-    
-        // req.query.from = Object.keys(languages)[Object.values(languages).indexOf(`${req.query.from}`)]; //getting the short form of the language
-        // req.query.to = Object.keys(languages)[Object.values(languages).indexOf(`${req.query.to}`)];
-    
+        
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
         await page.goto(`https://translate.google.com/#view=home&op=translate&sl=${req.query.from}&tl=${req.query.to}&text=${urlFromText}`);
@@ -23,4 +21,4 @@ app.get("/translate", async (req, res) => {
 })
 
 
-app.listen(3002, () => console.log("Server is listening on port 3002"))
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`))
